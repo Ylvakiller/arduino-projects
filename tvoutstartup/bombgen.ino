234cce1e923116f2cf774c66a1e282e74d4f43f5
Ylvakiller <rem.geuze@gmail.com>
2014-10-04 13:03:47 +0200

    finished numbers and made seperate files

---
+++ tvoutstartup/bombgen.ino
@@ -0,0 +1,297 @@
+
+
+void setBombs(){  
+  byte x = random(0,13);    
+  byte y = random(0,13);    
+  if(grid[x][y]==0){      
+    grid[x][y]==9; 
+  }
+}
+
+
+
+void countBombs(){
+  byte x = 0;
+  byte y = 0;
+  while (x<13){
+    while(y<13){
+      if(grid[x][y]!=9){
+        if(x==0){                    //left border
+          if (y==0){                 //top border
+            int i = 0;
+            if(count6(x, y)){
+              i++;
+            }
+            if(count8(x, y)){
+              i++;
+            }
+            if(count9(x, y)){
+              i++;
+            }
+            if(i==0){
+              grid[x][y] = 10;
+            }
+            else{
+              grid[x][y] = i;
+            }
+          }
+          else if(y==12){          //bottom border
+            int i = 0;
+            if(count2(x, y)){
+              i++;
+            }
+            if(count3(x, y)){
+              i++;
+            }
+            if(count6(x, y)){
+              i++;
+            }
+            if(i==0){
+              grid[x][y] = 10;
+            }
+            else{
+              grid[x][y] = i;
+            }
+          }
+          else{                    //no y border
+            int i = 0;
+            if(count2(x, y)){
+              i++;
+            }
+            if(count3(x, y)){
+              i++;
+            }
+            if(count6(x, y)){
+              i++;
+            }
+            if(count8(x, y)){
+              i++;
+            }
+            if(count9(x, y)){
+              i++;
+            }
+            if(i==0){
+              grid[x][y] = 10;
+            }
+            else{
+              grid[x][y] = i;
+            }
+          }
+        }
+        else if(x==12){             //right border
+          if (y==0){                //top border
+            int i = 0;
+            if(count4(x, y)){
+              i++;
+            }
+            if(count7(x, y)){
+              i++;
+            }
+            if(count8(x, y)){
+              i++;
+            }
+            if(i==0){
+              grid[x][y] = 10;
+            }
+            else{
+              grid[x][y] = i;
+            }
+          }
+          else if(y==12){          //bottom border
+            int i = 0;
+            if(count1(x, y)){
+              i++;
+            }
+            if(count2(x, y)){
+              i++;
+            }
+            if(count4(x, y)){
+              i++;
+            }
+            if(i==0){
+              grid[x][y] = 10;
+            }
+            else{
+              grid[x][y] = i;
+            }
+          }
+          else{                    //no y border
+            int i = 0;
+            if(count1(x, y)){
+              i++;
+            }
+            if(count2(x, y)){
+              i++;
+            }
+            if(count4(x, y)){
+              i++;
+            }
+            if(count7(x, y)){
+              i++;
+            }
+            if(count8(x, y)){
+              i++;
+            }
+            if(i==0){
+              grid[x][y] = 10;
+            }
+            else{
+              grid[x][y] = i;
+            }
+          }
+        }
+        else{                  //no x border
+          if (y==0){           //top border
+            int i = 0;
+            if(count1(x, y)){
+              i++;
+            }
+            if(count2(x, y)){
+              i++;
+            }
+            if(count3(x, y)){
+              i++;
+            }
+            if(count4(x, y)){
+              i++;
+            }
+            if(count6(x, y)){
+              i++;
+            }
+            if(i==0){
+              grid[x][y] = 10;
+            }
+            else{
+              grid[x][y] = i;
+            }
+          }
+          else if(y==12){            //bottom border
+            int i = 0;
+            if(count4(x, y)){
+              i++;
+            }
+            if(count6(x, y)){
+              i++;
+            }
+            if(count7(x, y)){
+              i++;
+            }
+            if(count8(x, y)){
+              i++;
+            }
+            if(count9(x, y)){
+              i++;
+            }
+            if(i==0){
+              grid[x][y] = 10;
+            }
+            else{
+              grid[x][y] = i;
+            }
+          }
+          else{                  //no y border
+            int i = 0;
+            if(count1(x, y)){
+              i++;
+            }
+            if(count2(x, y)){
+              i++;
+            }
+            if(count3(x, y)){
+              i++;
+            }
+            if(count4(x, y)){
+              i++;
+            }
+            if(count6(x, y)){
+              i++;
+            }
+            if(count7(x, y)){
+              i++;
+            }
+            if(count8(x, y)){
+              i++;
+            }
+            if(count9(x, y)){
+              i++;
+            }
+            if(i==0){
+              grid[x][y] = 10;
+            }
+            else{
+              grid[x][y] = i;
+            }
+          }
+        }
+      }
+      y++;
+    }
+    y=0;
+    x++;
+  }
+
+}
+
+
+
+
+
+
+boolean count1(byte x, byte y){
+  if(grid[x-1][y-1]==9){
+    return true;
+  }
+  return false;
+}
+
+boolean count2(byte x, byte y){
+  if(grid[x][y-1]==9){
+    return true;
+  }
+  return false;
+}
+
+boolean count3(byte x, byte y){
+  if(grid[x+1][y-1]==9){
+    return true;
+  }
+  return false;
+}
+
+boolean count4(byte x, byte y){
+  if(grid[x-1][y]==9){
+    return true;
+  }
+  return false;
+}
+
+
+boolean count6(byte x, byte y){
+  if(grid[x+1][y]==9){
+    return true;
+  }
+  return false;
+}
+
+boolean count7(byte x, byte y){
+  if(grid[x-1][y+1]==9){
+    return true;
+  }
+  return false;
+}
+
+
+boolean count8(byte x, byte y){
+  if(grid[x][y+1]==9){
+    return true;
+  }
+  return false;
+}
+
+boolean count9(byte x, byte y){
+  if(grid[x+1][y+1]==9){
+    return true;
+  }
+  return false;
+}
+
---
+++ tvoutstartup/numbers.ino
@@ -0,0 +1,84 @@
+void draw1(byte x, byte y){
+  clearSquare(x,y);
+  TV.draw_line(((x*7)+5),((y*7)+3), ((x*7)+5),((y*7)+8), WHITE);
+  TV.draw_line(((x*7)+6),((y*7)+3), ((x*7)+6),((y*7)+8), WHITE);
+  TV.set_pixel(((x*7)+4),((y*7)+4), WHITE);
+  TV.set_pixel(((x*7)+4),((y*7)+8),WHITE);
+  TV.set_pixel(((x*7)+7),((y*7)+8),WHITE);
+}
+
+void draw2(byte x, byte y){
+  clearSquare(x,y);
+  TV.set_pixel(((x*7)+4),((y*7)+4), WHITE);
+  TV.draw_line(((x*7)+5),((y*7)+3),((x*7)+7),((y*7)+3), WHITE);
+  TV.draw_line(((x*7)+7),((y*7)+4),((x*7)+7),((y*7)+5), WHITE);
+  TV.draw_line(((x*7)+5),((y*7)+6),((x*7)+7),((y*7)+6), WHITE);
+  TV.set_pixel(((x*7)+4),((y*7)+7), WHITE);
+  TV.draw_line(((x*7)+4),((y*7)+8),((x*7)+8),((y*7)+8), WHITE);
+}
+
+void draw3(byte x, byte y){
+  clearSquare(x,y);
+  TV.set_pixel(((x*7)+7),((y*7)+4), WHITE);
+  TV.set_pixel(((x*7)+7),((y*7)+6), WHITE);
+  TV.set_pixel(((x*7)+7),((y*7)+7), WHITE);
+  TV.draw_line(((x*7)+4),((y*7)+3),((x*7)+7),((y*7)+3), WHITE);
+  TV.draw_line(((x*7)+4),((y*7)+5),((x*7)+7),((y*7)+5), WHITE);
+  TV.draw_line(((x*7)+4),((y*7)+8),((x*7)+7),((y*7)+8), WHITE);
+}
+
+void draw4(byte x, byte y){
+  clearSquare(x,y);
+  TV.draw_line(((x*7)+4),((y*7)+3),((x*7)+4),((y*7)+6), WHITE);
+  TV.draw_line(((x*7)+7),((y*7)+3),((x*7)+7),((y*7)+8), WHITE);
+  TV.set_pixel(((x*7)+5),((y*7)+6), WHITE);
+  TV.set_pixel(((x*7)+6),((y*7)+6), WHITE);
+}
+
+void draw5(byte x, byte y){
+  clearSquare(x,y);
+  TV.draw_line(((x*7)+4),((y*7)+3),((x*7)+8),((y*7)+3), WHITE);
+  TV.draw_line(((x*7)+4),((y*7)+4),((x*7)+4),((y*7)+6), WHITE);
+  TV.draw_line(((x*7)+5),((y*7)+6),((x*7)+8),((y*7)+6), WHITE);
+  TV.set_pixel(((x*7)+7),((y*7)+7), WHITE);
+  TV.draw_line(((x*7)+4),((y*7)+8),((x*7)+8),((y*7)+8), WHITE);
+}
+
+void draw6(byte x, byte y){
+  clearSquare(x,y);
+  TV.draw_line(((x*7)+4),((y*7)+4),((x*7)+4),((y*7)+7), WHITE);
+  
+  TV.set_pixel(((x*7)+5),((y*7)+3),WHITE);
+  TV.set_pixel(((x*7)+6),((y*7)+3),WHITE);
+  
+  TV.set_pixel(((x*7)+5),((y*7)+5),WHITE);
+  TV.set_pixel(((x*7)+6),((y*7)+5),WHITE);
+  
+  TV.set_pixel(((x*7)+5),((y*7)+8),WHITE);
+  TV.set_pixel(((x*7)+6),((y*7)+8),WHITE);
+  
+  TV.set_pixel(((x*7)+7),((y*7)+6),WHITE);
+  TV.set_pixel(((x*7)+7),((y*7)+7),WHITE);  
+}
+
+void draw7(byte x, byte y){
+  clearSquare(x,y);
+  TV.draw_line(((x*7)+4),((y*7)+3),((x*7)+8),((y*7)+3), WHITE);
+  TV.draw_line(((x*7)+5),((y*7)+6),((x*7)+5),((y*7)+8), WHITE);
+  TV.set_pixel(((x*7)+6),((y*7)+5), WHITE);
+  TV.set_pixel(((x*7)+7),((y*7)+4), WHITE);
+}
+
+void draw8(byte x, byte y){
+  clearSquare(x,y);
+  TV.draw_line(((x*7)+4),((y*7)+3), ((x*7)+7),((y*7)+3), WHITE);
+  TV.draw_line(((x*7)+4),((y*7)+5), ((x*7)+7),((y*7)+5), WHITE);
+  TV.draw_line(((x*7)+4),((y*7)+8), ((x*7)+7),((y*7)+8), WHITE);
+  TV.set_pixel(((x*7)+3),((y*7)+4), WHITE);
+  TV.set_pixel(((x*7)+7),((y*7)+4), WHITE);
+  TV.set_pixel(((x*7)+3),((y*7)+6), WHITE);
+  TV.set_pixel(((x*7)+7),((y*7)+6), WHITE);
+  TV.set_pixel(((x*7)+3),((y*7)+7), WHITE);
+  TV.set_pixel(((x*7)+7),((y*7)+7), WHITE);
+}
+
