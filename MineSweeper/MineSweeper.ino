93eed9efdc06b0f666aeba891f50034e25058623
Ylvakiller <rem.geuze@gmail.com>
2014-10-17 15:28:22 +0200

    Fully implemented the button

--- MineSweeper/MineSweeper.ino
+++ MineSweeper/MineSweeper.ino
@@ -5,8 +5,8 @@
 #include <TVout.h>
 TVout TV;
 
-#include <pollserial.h>
-pollserial pserial;
+//#include <pollserial.h>
+//pollserial pserial;
 
 byte cursorX;
 byte cursorY;
@@ -14,6 +14,7 @@ byte cposX;
 byte cposY;
 byte bombsLeft;
 unsigned long timestamp;
+boolean allowButton;
 
 byte grid[13][13] = {
   0};
@@ -35,7 +36,7 @@ void setup(){
   cursorX=1;
   cposX=0;
   cposY=0;
-  TV.set_hbi_hook(pserial.begin(57600));
+  //TV.set_hbi_hook(pserial.begin(57600));
 }
 
 void loop(){
@@ -49,19 +50,36 @@ void loop(){
   screen();
   initialGrid();
   countBombs();
-  drawDigit2(0);
-  boolean temp1=false;
-  boolean temp2 = false;
-  unsigned long oldTimeStamp;
-  while (1){
+  boolean temp1,temp2,temp2old;
+  while (1){  
     //drawGen();
-
+    temp2old=temp2;
     getCursorMovement();
-    if (button()){
-      if(millis()-oldTimeStamp<300){    //long press
-        
-      }else{                            //short press
+    if(allowButton){                  //if this is true then the joystick is not on one of the sides, should stop false short presses
+      if (button()){
+        if(millis()-timestamp>300){    //long press
+          temp1=true;                  //temp1 defines a long or a short press
+          temp2=true;
+          drawDigit1(1);
+        }
+        else{  //short press, made to debounce when moving the cursor
+          temp1=false;
+          temp2=true;                //means button is pressed
+          drawDigit1(2);
+        }
+      }
+      else{
+        temp2=false;              //means button is not pressed
+      }
 
+      if(temp2old&&!temp2){        //means button is just released          
+        if(temp1){
+
+          longPress();
+        }
+        else{
+          shortPress();
+        }
       }
     }
   }
@@ -130,23 +148,29 @@ void getCursorMovement(){
   int temp = analogRead(A0);
   if(temp<200){
     cursorX = 0;
+    allowButton=false;
   }
   else if (temp>400&&temp<600){
     cursorX=1;
+    allowButton=true;
   }
   else if (temp>900){
     cursorX=2;
+    allowButton=false;
   }
 
   temp = analogRead(A1);
   if(temp<200){
     cursorY = 0;
+    allowButton=false;
   }
   else if (temp>400&&temp<600){
     cursorY=1;
+    allowButton=true;
   }
   else if (temp>900){
     cursorY=2;
+    allowButton=false;
   }
 
   if(oldCursorX!=cursorX){
@@ -181,4 +205,207 @@ boolean button(){
     return false;
   }
 }
+
+//returns true if the current position of the cursor is on a flag
+boolean flaggedPosition(){
+  byte t=grid[cposX][cposY];
+  if(t>=20&&t<=29){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+boolean coveredPosition(){
+  byte t=grid[cposX][cposY];
+  if(t>=1&&t<=10){
+    return true;
+  }
+  else{
+    return false;
+  }  
+}
+
+boolean unCoveredPosition(){
+  byte t=grid[cposX][cposY];
+  if(t>=11&&t<=19){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
+//Code that needs to be excecuted when a short button press is happening
+void shortPress(){
+  if(unCoveredPosition()){
+    //do nothing, flags cannot be placed on covered positions
+  }
+  else if (flaggedPosition()){
+    //remove flag
+    removeFlag();
+  }
+  else{
+    //place flag
+    placeFlag();
+  }
+}
+
+//code that needs to be executed in case of a long button press
+void longPress(){
+  if(unCoveredPosition()){
+    // do nothing, position is already uncovered
+  }
+  else if (flaggedPosition()){
+    //More nothing, you cannot clear a flagged area
+  }
+  else {
+    removeCover();
+  }
+}
+
+
+void removeCover(){
+  switch (grid[cposX][cposY]){
+  case 1:
+    grid[cposX][cposY]=11;
+    draw1(cposX,cposY);
+    break;
+  case 2:
+    grid[cposX][cposY]=12;
+    draw2(cposX,cposY);
+    break;
+  case 3:
+    grid[cposX][cposY]=13;
+    draw3(cposX,cposY);
+    break;
+  case 4:
+    grid[cposX][cposY]=14;
+    draw4(cposX,cposY);
+    break;
+  case 5:
+    grid[cposX][cposY]=15;
+    draw5(cposX,cposY);
+    break;
+  case 6:
+    grid[cposX][cposY]=16;
+    draw6(cposX,cposY);
+    break;
+  case 7:
+    grid[cposX][cposY]=17;
+    draw7(cposX,cposY);
+    break;
+  case 8:
+    grid[cposX][cposY]=18;
+    draw8(cposX,cposY);
+    break;
+  case 9:
+    grid[cposX][cposY]=30;          //bomb
+    bombExplosion();
+    break;
+  case 10:
+    grid[cposX][cposY]=19;
+    drawEmpty(cposX,cposY);
+    break;
+  }
+}
+
+void placeFlag(){
+  switch (grid[cposX][cposY]){
+  case 1:
+    grid[cposX][cposY]=21;
+    drawFlag(cposX,cposY);
+    break;
+  case 2:
+    grid[cposX][cposY]=22;
+    drawFlag(cposX,cposY);
+    break;
+  case 3:
+    grid[cposX][cposY]=23;
+    drawFlag(cposX,cposY);
+    break;
+  case 4:
+    grid[cposX][cposY]=24;
+    drawFlag(cposX,cposY);
+    break;
+  case 5:
+    grid[cposX][cposY]=25;
+    drawFlag(cposX,cposY);
+    break;
+  case 6:
+    grid[cposX][cposY]=26;
+    drawFlag(cposX,cposY);
+    break;
+  case 7:
+    grid[cposX][cposY]=27;
+    drawFlag(cposX,cposY);
+    break;
+  case 8:
+    grid[cposX][cposY]=28;
+    drawFlag(cposX,cposY);
+    break;
+  case 9:
+    grid[cposX][cposY]=29;          //bomb
+    drawFlag(cposX,cposY);
+    break;
+  case 10:
+    grid[cposX][cposY]=20;
+    drawFlag(cposX,cposY);
+    break;
+  }
+}
+
+void removeFlag(){
+  switch (grid[cposX][cposY]){
+  case 21:
+    grid[cposX][cposY]=1;
+    drawSquare(cposX,cposY);
+    break;
+  case 22:
+    grid[cposX][cposY]=2;
+    drawSquare(cposX,cposY);
+    break;
+  case 23:
+    grid[cposX][cposY]=3;
+    drawSquare(cposX,cposY);
+    break;
+  case 24:
+    grid[cposX][cposY]=4;
+    drawSquare(cposX,cposY);
+    break;
+  case 25:
+    grid[cposX][cposY]=5;
+    drawSquare(cposX,cposY);
+    break;
+  case 26:
+    grid[cposX][cposY]=6;
+    drawSquare(cposX,cposY);
+    break;
+  case 27:
+    grid[cposX][cposY]=7;
+    drawSquare(cposX,cposY);
+    break;
+  case 28:
+    grid[cposX][cposY]=8;
+    drawSquare(cposX,cposY);
+    break;
+  case 29:
+    grid[cposX][cposY]=9;          //bomb
+    drawSquare(cposX,cposY);
+    break;
+  case 20:
+    grid[cposX][cposY]=10;
+    drawSquare(cposX,cposY);
+    break;
+  }
+}
+
+
+void bombExplosion(){
+  //game over
+}
+
+
 
