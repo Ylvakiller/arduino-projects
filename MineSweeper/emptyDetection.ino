6fa295aa9c37bb62d23ffb43d60d0059484ac45b
Ylvakiller <rem.geuze@gmail.com>
2014-10-19 22:19:54 +0200

    Wrote most of the blobDetection UNTESTED!!

--- MineSweeper/emptyDetection.ino
+++ MineSweeper/emptyDetection.ino
@@ -1,4 +1,5 @@
-byte empty[13][13] = {0};      //empty array
+byte emptyArray[13][13] = {
+  0};      //empty array
 
 void CalculateEmptyArray(){
   byte x = 0;
@@ -8,55 +9,65 @@ void CalculateEmptyArray(){
     while(y<13){
       if(grid[x][y]!=10){
         if(x==0&&y==0){
-          empty[x][y]=i;        //top left corner
+          emptyArray[x][y]=i;        //top left corner
           i++;
-        }else if (x==0){        //left border
-          if (check3[x][y]){
-            empty[x][y] = empty[x][y-1];
-          }else{
-             
+        }
+        else if (x==0){        //left border
+          if (checkTop(x,y)){
+            emptyArray[x][y] = emptyArray[x][y-1];
+          }
+          else{
+            emptyArray[x][y]=i;
+            i++;
           }
         }
+        else if (y==0){      //top border
+          if (checkLeft(x,y)){
+            emptyArray[x][y] = emptyArray[x-1][y];
+          }
+          else{
+            emptyArray[x][y] = i;
+          }
+        }
+        else{               //no borders on left or on top
+          if(checkLeft(x,y)&&checkTop(x,y)){    //blobs both on the top and on the bottom
+            emptyArray[x][y] = emptyArray[x][y-1];                //saves the position of the top blob on the place of the intersection
+            transformBlob(emptyArray[x][y-1],emptyArray[x-1][y]); //changes all the values in the empty array that have the value of the left blob towards the value fo the top blob
+          }
+          else if(checkLeft(x,y)){              //blob on the left but not on the top
+            emptyArray[x][y] = emptyArray[x-1][y];
+          }
+          else if (checkTop(x,y)){
+            emptyArray[x][y] = emptyArray[x][y-1];        //blob on the top but not on the right
+          }
+          else{                                //No blob found
+            emptyArray[x][y] = i;
+            i++;
+          }
         }
       }
-      y++;
     }
-    y=0;
-    x++;
+    y++;
   }
-
+  y=0;
+  x++;
 }
 
-boolean check1(byte x, byte y){
-  //checks at the numerical position 1
-}
 
-boolean check2(byte x, byte y){
+
+
+boolean checkLeft(byte x, byte y){
   //checks at the numerical position 2
 }
 
-boolean check3(byte x, byte y){
+boolean checkTop(byte x, byte y){
   //checks at the numerical position 3
 }
 
-boolean check4(byte x, byte y){
-  //checks at the numerical position 4
+//Changes all the values in the empty array that have a value of second towards the value of first
+void transformBlob(byte first,byte second){
 }
 
-boolean check6(byte x, byte y){
-  //checks at the numerical position 6
-}
 
-boolean check7(byte x, byte y){
-  //checks at the numerical position 7
-}
-
-boolean check8(byte x, byte y){
-  //checks at the numerical position 8
-}
-
-boolean check9(byte x, byte y){
-  //checks at the numerical position 9
-}
 
 
